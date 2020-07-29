182e81a82f93c76efeb74545f151d3d1bd933419
YeungShaoFeng <2013114698@qq.com>
2020-07-29 22:19:18 +0800

    better PASSWORD

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 in_file="./include/PI64"
 en_file="./out/encrypted.badway"
 de_file="./out/decrypted.badway"
-PASSWORD="e6b35253f56cecf20d827c621f2c107e8755f54896f8062f263d24ca5092ee6a"
+PASSWORD="e6b35253f56cecf2"
 log_level="4"
 
 # make the $in_file.
@@ -36,3 +36,14 @@ echo "============================================================="
 # clean up.
 #rm -rf $diff_dir $in_file.h
 #rm -rf CMakeCache.txt Makefile cmake-build-debug out CMakeFiles cmake_install.cmake badway
+
+
+# Linux localhost 4.14.117-perf-g509c074 #1 SMP PREEMPT Wed Apr 8 03:32:01 CST 2020 arrch64 Android
+OS=`uname -a`
+if [ `echo $OS | grep -c "cen" ` -gt 0 ]
+then echo "Success"
+elif [ `echo $OS | grep -c "Darwin" ` -gt 0 ]
+then echo "Darwin"
+elif [ `echo $OS | grep -c "cen" ` -gt 0 ]
+then echo "Success"
+fi
